12110e428dc81d55077b0910171171293e359f88
Piktato <ardi.ramushi@gmail.com>
2016-09-27 14:10:42 +0200

    Menu maison fini

--- Assets/menuResidence.cs
+++ Assets/menuResidence.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class menuResidence : MonoBehaviour {
 
     public GameObject residences;
+    
 	// Use this for initialization
 	void Start () {
 	
@@ -16,8 +17,16 @@ public class menuResidence : MonoBehaviour {
     public void OnMouseDown()
     {
         Debug.Log("Touché");
-        residences = Instantiate(residences,Vector3.zero, Quaternion.identity)as GameObject;
-        residences.transform.parent = GameObject.Find("Menu").transform;
+        if ((GameObject.FindGameObjectWithTag("MenuGris") == null))
+        {
+           GameObject LeClone = Instantiate(residences, Vector3.zero, Quaternion.identity) as GameObject;
+            LeClone.transform.parent = GameObject.Find("Menu").transform;
+            LeClone.transform.localScale = new Vector3(0.02f, 0.3f, 0);
+            LeClone.transform.localPosition = new Vector3(0.05f, 0.5f, 0);
+        }
+        else
+        {
+            Destroy(GameObject.FindGameObjectWithTag("MenuGris"));
+        }
     }
-    
-}
+}///
