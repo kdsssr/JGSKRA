9b73d2c5e7e6f5f5ba9bf60c17b58cdcbce61805
kdsssr <kevin.dsssr@eduge.ch>
2016-09-27 16:00:20 +0200

    Finitions

--- Assets/menuResidence.cs
+++ Assets/menuResidence.cs
@@ -17,16 +17,16 @@ public class menuResidence : MonoBehaviour {
     public void OnMouseDown()
     {
         Debug.Log("Touché");
-        if ((GameObject.FindGameObjectWithTag("MenuGris") == null))
+        if ((GameObject.FindGameObjectWithTag("MenuResidence") == null))
         {
-           GameObject LeClone = Instantiate(residences, Vector3.zero, Quaternion.identity) as GameObject;
+            GameObject LeClone = Instantiate(residences, Vector3.zero, Quaternion.identity) as GameObject;
             LeClone.transform.parent = GameObject.Find("Menu").transform;
             LeClone.transform.localScale = new Vector3(0.02f, 0.3f, 0);
-            LeClone.transform.localPosition = new Vector3(0.05f, 0.5f, 0);
+            LeClone.transform.localPosition = new Vector3(0.05f, 0.25f, 0);
         }
         else
         {
-            Destroy(GameObject.FindGameObjectWithTag("MenuGris"));
+            Destroy(GameObject.FindGameObjectWithTag("MenuResidence"));
         }
     }
 }///
